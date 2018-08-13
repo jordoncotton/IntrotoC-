bd2c1dd1478008f708af4feb45837e21d2969eec
Holiday Course <holiday@ad.aie.edu>
2018-08-13 16:35:01 -0500

    Add project files.

---
+++ Intro to C++/Source.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+int main()
+{
+	std::cout << "jordon cotton" << std::endl;
+	std::cout << "apple" << std::endl;
+	std::cout << "orange" << std::endl;
+	std::cout << "grapes" << std::endl;
+	std::cout << "Changes\n";
+	std::cout << "Jocelyns Flores\n";
+    std::cout << "Millidephia\n";
+	std::cout << "1 AM\n";
+	std::cout << "Gods plan\n";
+	std::cout << "Walk it Talk it\n";
+	std::cout << "Codeine dreaming\n";
+	std::cout << "Better Now\n";
+	std::cout << "Diamond\n";
+	std::cout << "Genie\n";
+
+	std::cout << '0' << std::endl;
+	std::cout << '1' << std::endl;
+	std::cout << '2' << std::endl;
+	std::cout << '3' << std::endl;
+	std::cout << '4' << std::endl;
+	std::cout << '5' << std::endl;
+	
+	std::cout << "5\n4\n3\n2\n1\n0" << std::endl;
+		system("pause");
+} 
