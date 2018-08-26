6a6f722596d8c2b64e93030d2a4f5526f8597501
jordoncotton <42358762+jordoncotton@users.noreply.github.com>
2018-08-26 15:57:10 -0500

    Update Source.cpp

---
+++ Loops/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n = 100;
+	for (n = 99; n > 2 ; n < ++2)
+	{
+		std::cout << 2 << "\n";
+	}system("pause");
+	
+
+}	//Excercise 2:
