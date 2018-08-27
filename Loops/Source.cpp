877549c020ac9a938328ff8b905e5dabd33eaa3d
jordoncotton <42358762+jordoncotton@users.noreply.github.com>
2018-08-27 08:25:24 -0500

    Update Source.cpp

--- Loops/Source.cpp
+++ Loops/Source.cpp
@@ -1,14 +1,83 @@
-#include <iostream>
-
-using namespace std;
+#include <iostream>;
 
 int main()
 {
-	int n = 100;
-	for (n = 99; n > 2 ; n < ++2)
+	// 1>) part A
 	{
-		std::cout << 2 << "\n";
-	}system("pause");
-	
+		for (int i = 100; i >= 0; i--)
+		{
+			std::cout << i << std::endl;
+		}
 
-}	//Excercise 2:
+	}
+
+	// 1.) part B
+	{
+		for (int i = 99; i >= 0; i--)
+		{
+			std::cout << i << std::endl;
+		}
+
+	}
+
+	// 1.) part C
+	{
+		for (int i = 100; i >= 1; i--)
+		{
+			std::cout << i << std::endl;
+		}
+
+	}
+
+	//1.) part D
+	{
+		for (int i = 100; i >= 1; i -= 2)
+		{
+			std::cout << i << std::endl;
+		}
+
+	}
+
+	//2.) 
+	for (int i = 0; i <= 100; i++)
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			std::cout << "Fizz Buzz" << std::endl;
+
+		}
+		else if (i % 3 == 0)
+		{
+			std::cout << "Fizz" << std::endl;
+
+		}
+		else if (i % 5 == 0)
+		{
+			std::cout << "Buzz" << std::endl;
+		}
+		else
+		{
+			std::cout << i << std::endl;
+		}
+
+	//3)
+	{
+		{
+			int x = 0;
+			int y = 0;
+
+			while (y <= 1000)
+			{
+				if (y % 3 == 0 && y % 5 == 0)
+				{
+					x += y;
+				}
+				y++;
+			}
+			std::cout << x << std::endl;
+		}
+		break;
+
+	}
+
+	system("pause");
+}
