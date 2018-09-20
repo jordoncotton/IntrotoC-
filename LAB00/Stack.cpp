5d10ca6ff94484390bf5450b5930e6fbf0566526
jordoncotton1 <jordoncotton1@gmail.com>
2018-09-20 00:20:25 -0500

    IronMan vs Captainpoopoo

--- LAB00/Stack.cpp
+++ LAB00/Stack.cpp
@@ -2,11 +2,18 @@
 
 Hero Stack::top()
 {
+	if (!IsEmpty())
+		return mData[mCount];
 	return Hero();
 }
 
 bool Stack::pop()
 {
+	if (!IsEmpty())
+	{
+		mCount = mCount - 1;
+		return true;
+	}
 	return false;
 }
 
@@ -17,10 +24,12 @@ bool Stack::push(Hero *)
 
 bool Stack::IsEmpty()
 {
+	return mCount == 0;
 	return false;
 }
 
 bool Stack::IsFull()
 {
+	return mCount == 20;
 	return false;
 }
