35f641d0c36482e39e00928cdffd43ee92619f89
Jordon Cotton <s188044@students.aie.edu>
2018-10-15 17:47:07 -0500

    link list

--- linked list/Iterator.h
+++ linked list/Iterator.h
@@ -6,10 +6,39 @@ template <typename I>
 
 class Iterator
 {
+public:
 	Iterator();
-	Iterator(nodeType<I>);
+	Iterator(nodeType<I>*ptr);
 	I operator*();
 	Iterator operator++();
 	const bool operator==(const Iterator <I>&();
 	const bool operator!=(const Iterator <I>&();
+
+private:
+	nodeType<I>* current;
 };
+
+template<typename I>
+inline Iterator<I>::Iterator()
+{
+
+}
+
+template<typename I>
+inline Iterator<I>::Iterator(nodeType<I>* ptr)
+{
+
+}
+
+template<typename I>
+Iterator<I>::operator*()
+{
+	current = current->next;
+	return *this;
+}
+
+template<typename I>
+Iterator Iterator<I>::operator++()
+{
+	return Iterator operator++;
+}
--- linked list/main.cpp
+++ linked list/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include "link.h"
 #include "node.h"
+#include "Iterator.h"
 
 int main()
 {
+	nodeType<int> I;
+	nodeType<int> L;
 
+	 
 }
